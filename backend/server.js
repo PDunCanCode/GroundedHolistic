eb2f65747b9b42f3abc70548ff6cc68de0e4bd99
pDunCanCode <chesiredarling@gmail.com>
2020-11-18 13:58:20 -0800

    add mongo and connect DB with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const { default: products } = require("../frontend/src/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
