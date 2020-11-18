c071c92d40f7cd1368720e8f61553b93a41fdd82
pDunCanCode <chesiredarling@gmail.com>
2020-11-17 20:41:17 -0800

    add express, and simple routes/ backend

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "General Hollistic Health Consultation and Evaluation",
+    image: "/images/jon.jpg",
+    description:
+      "An introductory evaluation of the client's baseline overall hollistic mental and physicall health. Typically includes two written evaluations, and one hour of one-on-one time with Jonathan to discuss findings.",
+    brand: "Grounded",
+    category: "Evaluation",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Still Mediation Session",
+    image: "/images/stoned.jpg",
+    description:
+      "The ultimate life experience begins with you, in your own head. A transformative meditation session that adds tons of value without complexity. An unprecedented leap in inner peace.",
+    brand: "Grounded Energetics",
+    category: "Meditation",
+    price: 59.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Active Meditation Session",
+    image: "/images/activeMed.jpg",
+    description:
+      "Characterized by versatile and dynamic movement, active meditation further clarifies itself using a set of robust energy-focusing then emergy-releasing system paradigms",
+    brand: "Osho",
+    category: "Meditation",
+    price: 59.99,
+    countInStock: 5,
+    rating: 4.3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Snowboard Instruction",
+    image: "/images/snowWalk.jpg",
+    description:
+      "The ultimate snowboard session starts with Jonathan Duncan. Whether you are into park, slalom, freeride, or just starting out, there is a package and strategy to guide your progress. Jonathan is instructing at China Peak this 20-21 winter season.",
+    brand: "Duncan/ ChinaPeak",
+    category: "Instruction",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Woodsman Workout Weekend",
+    image: "/images/outdoor.jpg",
+    description:
+      "Get a better handle on your outdoor skillset with a weekend camp sesh. The 2, or 3, day experience will focus on building your outdoor/ bushman abilities. Including axe-work, foraging, fire and camp building. ",
+    brand: "Grounded",
+    category: "Instruction",
+    price: 749.99,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Bondfire Cultivation",
+    image: "/images/bondfire.jpg",
+    description:
+      "Meet the 'Bondfire' - Our most popular social event begins with intentional fire engineering and design. Fire design typically takes 8-10 hours. All fires start with a single match and are built to be enjoyed for 3-4 hours in a group setting. ",
+    brand: "Grounded",
+    category: "Consultation",
+    price: 229.99,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
