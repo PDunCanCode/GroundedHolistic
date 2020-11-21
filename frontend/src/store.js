feb9308d4d74bb1770205cb77a04294a2b2c0932
pDunCanCode <chesiredarling@gmail.com>
2020-11-21 09:35:23 -0800

    bring in actiona and reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { productListReducer } from "./reducers/productReducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productsList: productListReducer,
+});
 
 const initialState = {};
 
@@ -11,7 +14,7 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware([...middleware]))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
