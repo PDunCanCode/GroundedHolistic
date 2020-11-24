67b4db98c8c60d086cb202226823ac460d41d5a0
pDunCanCode <chesiredarling@gmail.com>
2020-11-23 16:34:20 -0800

    add userUpdateProfile actions/constants/reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,6 +9,7 @@ import {
   userDetailsReducer,
   userLoginReducer,
   userRegisterReducer,
+  userUpdateProfileReducer,
 } from "./reducers/userReducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
@@ -19,6 +20,7 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
